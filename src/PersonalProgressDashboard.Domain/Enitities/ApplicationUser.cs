7ceb0a6330faeb46a874e32a2847853e1a963189
layla-p <layla.porter@gmail.com>
2017-10-08 16:00:03 +0100

    added in user class and db context

---
+++ src/PersonalProgressDashboard.Domain/Enitities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace PersonalProgressDashboard.Domain.Enitities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float WeightInKg { get; set; }
+        public bool IsMetric { get; set; } = true;
+        public List<string> Mantras { get; set; } = new List<string>();
+    }
+}
