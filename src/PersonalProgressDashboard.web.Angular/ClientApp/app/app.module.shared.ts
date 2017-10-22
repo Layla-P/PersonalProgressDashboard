86e35b995338b910b1003ddc33e70c82ac5b38a3
layla-p <layla.porter@gmail.com>
2017-10-22 18:46:45 +0100

    angular app is posting and getting - home pages updated

--- src/PersonalProgressDashboard.web.Angular/ClientApp/app/app.module.shared.ts
+++ src/PersonalProgressDashboard.web.Angular/ClientApp/app/app.module.shared.ts
@@ -1,34 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { GoalsComponent } from './components/goals/goals.component';
+import { AddGoalsComponent } from './components/addgoals/addgoals.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         HomeComponent,
-        GoalsComponent
+        GoalsComponent,
+        AddGoalsComponent
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetchdata', component: FetchDataComponent },
+            { path: 'addgoal', component: AddGoalsComponent },
             { path: 'goals', component: GoalsComponent },
             { path: '**', redirectTo: 'home' }
         ])
--- src/PersonalProgressDashboard.web.Angular/ClientApp/app/components/goals/goals.component.ts
+++ src/PersonalProgressDashboard.web.Angular/ClientApp/app/components/goals/goals.component.ts
@@ -10,7 +10,7 @@ export class GoalsComponent {
     public goals: Goal[];
 
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        http.get('http://localhost:53330/api/PersonalGoals').subscribe(result => {
+        http.get('http://personal-progress-dashboard-api.azurewebsites.net/api/PersonalGoals').subscribe(result => {
             console.log(result);
             this.goals = result.json() as Goal[];
         }, error => console.error(error));
