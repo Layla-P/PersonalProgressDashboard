5e1ec21a293c3140030631ad03fbf8f29a531c15
layla-p <layla.porter@gmail.com>
2017-10-22 13:46:14 +0100

    updated typescript to 2.5.3

--- src/PersonalProgressDashboard.web.Angular/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ src/PersonalProgressDashboard.web.Angular/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
-    selector: 'fetchdata',
+    selector: 'fetch-data',
     templateUrl: './fetchdata.component.html'
 })
 export class FetchDataComponent {
