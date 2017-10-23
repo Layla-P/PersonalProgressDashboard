0187470f048ea75a1a81e34e57f83edf9871ebbf
layla-p <layla.porter@gmail.com>
2017-10-23 20:20:06 +0100

    added 'common' project, with a first draft of logginghelper.  Also sample logging usage in TokenApiController

--- src/PersonalProgressDashboard.Api/Controllers/TokenApiController.cs
+++ src/PersonalProgressDashboard.Api/Controllers/TokenApiController.cs
@@ -14,72 +14,77 @@ using PersonalProgressDashboard.Api.Middleware.Models;
 using PersonalProgressDashboard.Api.ViewModels;
 using PersonalProgressDashboard.Domain.Enitities;
 using Remotion.Linq.Utilities;
+using PersonalProgressDashboard.Common.Logging;
 
 namespace PersonalProgressDashboard.Api.Controllers
 {
 
-  [Route("api/[controller]")]
-  public class TokenApiController : Controller
-  {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly IPasswordHasher<ApplicationUser> _passwordHasher;
-    private readonly IOptions<AppOptions> _options;
-
-    public TokenApiController(
-      UserManager<ApplicationUser> userManager, IPasswordHasher<ApplicationUser> passwordHasher, IOptions<AppOptions> options)
+    [Route("api/[controller]")]
+    public class TokenApiController : Controller
     {
-      _userManager = userManager;
-      _passwordHasher = passwordHasher;
-      _options = options;
-    }
-    [AllowAnonymous]
-    [HttpPost]
-    public async Task<IActionResult> Post([FromBody] LoginViewModel model)
-    {
-      try
-      {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IPasswordHasher<ApplicationUser> _passwordHasher;
+        private readonly IOptions<AppOptions> _options;
 
-        var user = await _userManager.FindByNameAsync(model.Email);
-        if (user == null)
+        public TokenApiController(
+          UserManager<ApplicationUser> userManager, IPasswordHasher<ApplicationUser> passwordHasher, IOptions<AppOptions> options)
         {
-          return Unauthorized();
+            _userManager = userManager;
+            _passwordHasher = passwordHasher;
+            _options = options;
         }
-        if (_passwordHasher.VerifyHashedPassword(user, user.PasswordHash, model.Password) ==
-            PasswordVerificationResult.Success)
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] LoginViewModel model)
         {
-          var userClaims = await _userManager.GetClaimsAsync(user);
+            try
+            {
+                LogHelper.Debug($"[TokenApiController].[Post] Begin controller post, getting Auth Token");
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user == null)
+                {
+                    return Unauthorized();
+                }
+                if (_passwordHasher.VerifyHashedPassword(user, user.PasswordHash, model.Password) ==
+                    PasswordVerificationResult.Success)
+                {
+                    var userClaims = await _userManager.GetClaimsAsync(user);
 
-          var claims = new[]
-          {
-            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Email, user.Email)
-          }.Union(userClaims);
-          var symmetricSecurityKey =
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Value.JwtSecurityTokenKey));
-          var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Email, user.Email)
+                    }.Union(userClaims);
 
-          var token = new JwtSecurityToken(
-            issuer: _options.Value.JwtSecurityTokenIssuer,
-            audience: _options.Value.JwtSecurityTokenAudience,
-            claims: claims,
-            notBefore: DateTime.Now,
-            expires: DateTime.Now.AddDays(1),
-            signingCredentials: signingCredentials
-          );
-          return Ok(new
-          {
-            token = new JwtSecurityTokenHandler().WriteToken(token),
-            expiration = token.ValidTo
-          });
+                    var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Value.JwtSecurityTokenKey));
+                    var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                      issuer: _options.Value.JwtSecurityTokenIssuer,
+                      audience: _options.Value.JwtSecurityTokenAudience,
+                      claims: claims,
+                      notBefore: DateTime.Now,
+                      expires: DateTime.Now.AddDays(1),
+                      signingCredentials: signingCredentials
+                    );
+
+                    return Ok(new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    });
+                }
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error($"[TokenApiController].[Post] Exception : '{ex.Message}'");
+            }
+
+            return Unauthorized();
         }
-        return Unauthorized();
-      }
-      catch (Exception ex)
-      {
-         //todo
-      }
-      return Unauthorized();
     }
-  }
 }
